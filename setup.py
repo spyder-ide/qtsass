4a28f38ff07fbc41c311145d7802d5ccc40e346f
Yann Lanthony <yann.lanthony@gmail.com>
2015-08-16 14:51:12 +0200

    [Build] Force inclusion of msvcp120.dll (not copied by include_mscvr)

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {
     'include_msvcr': True,
+    'bin_includes': ['msvcp120.dll'],  # Force msvcp120 to be included (libsass dependency, not copied by include_mscvr)
     'optimize': 2
 }
 
 setup(
     name="qtsass",
     description="Compile a SCSS file to a valid Qt CSS.",
-    executables=[Executable("qtsass/qtsass.py")],
+    executables=[Executable("qtsass/qtsass.py", includes=['ctypes'])],
     options={'build_exe': build_exe_options},
     requires=['libsass', 'cx_Freeze'],
 )
