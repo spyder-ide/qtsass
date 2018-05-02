9b976b3e942d72b87c8cea3bf12fabf6102a5ceb
Dan Bradham <danielbradham@gmail.com>
2018-05-02 13:36:37 -0400

    Added boiler to tests

--- test_qtsass.py
+++ test_qtsass.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015 Yann Lanthony
+# Copyright (c) 2017-2018 Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (See LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Test suite for qtsass."""
+
+# Standard library imports
 from __future__ import absolute_import
 import unittest
 from textwrap import dedent
+
+# Local imports
 from qtsass.qtsass import NotConformer, QLinearGradientConformer
 
 
