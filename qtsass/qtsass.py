488f28f46299b90636a639d300719d7448e426d9
Dan Bradham <danielbradham@gmail.com>
2018-05-02 13:04:37 -0400

    Use single quotes - spyder style

--- qtsass/qtsass.py
+++ qtsass/qtsass.py
@@ -52,10 +52,10 @@ class QLinearGradientConformer(Conformer):
     """Conform QSS qlineargradient function."""
 
     qss_pattern = re.compile(
-        "qlineargradient\("
-        "((?:(?:\s+)?(?:x1|y1|x2|y2):(?:\s+)?[0-9A-Za-z$_-]+,?)+)"  # coords
-        "((?:(?:\s+)?stop:.*,?)+(?:\s+)?)?"  # stops
-        "\)",
+        'qlineargradient\('
+        '((?:(?:\s+)?(?:x1|y1|x2|y2):(?:\s+)?[0-9A-Za-z$_-]+,?)+)'  # coords
+        '((?:(?:\s+)?stop:.*,?)+(?:\s+)?)?'  # stops
+        '\)',
         re.MULTILINE
     )
 
@@ -68,11 +68,11 @@ class QLinearGradientConformer(Conformer):
         'stop: 0 red, stop: 1 blue' => '0 red, 1 blue'
         """
         new_group = []
-        for part in group.strip().split(","):
+        for part in group.strip().split(','):
             if part:
-                _, value = part.split(":")
+                _, value = part.split(':')
                 new_group.append(value.strip())
-        return ", ".join(new_group)
+        return ', '.join(new_group)
 
     def to_css(self, qss):
         """
@@ -95,7 +95,7 @@ class QLinearGradientConformer(Conformer):
             if not stops:
                 continue
 
-            new_stops = ", ({})".format(self._conform_group_to_css(stops))
+            new_stops = ', ({})'.format(self._conform_group_to_css(stops))
             conformed = conformed.replace(stops, new_stops, 1)
 
         return conformed
@@ -178,7 +178,7 @@ def qss_importer(where):
 
 
 def rgba(r, g, b, a):
-    result = "rgba({}, {}, {}, {}%)"
+    result = 'rgba({}, {}, {}, {}%)'
     if isinstance(r, sass.SassNumber):
         return result.format(
             int(r.value),
@@ -210,24 +210,24 @@ def qlineargradient(x1, y1, x2, y2, stops):
     :type stops: sass.SassList
     :return:
     """
-    stops_str = ""
+    stops_str = ''
     for stop in stops[0]:
         pos, color = stop[0]
-        stops_str += " stop: {} {}".format(pos.value, rgba_from_color(color))
+        stops_str += ' stop: {} {}'.format(pos.value, rgba_from_color(color))
 
-    return "qlineargradient(x1: {}, y1: {}, x2: {}, y2: {},{})".format(
+    return 'qlineargradient(x1: {}, y1: {}, x2: {}, y2: {},{})'.format(
         x1.value,
         y1.value,
         x2.value,
         y2.value,
-        stops_str.rstrip(",")
+        stops_str.rstrip(',')
     )
 
 
 def compile_to_css(input_file):
-    logger.debug("Compiling {}...".format(input_file))
+    logger.debug('Compiling {}...'.format(input_file))
 
-    with open(input_file, "r") as f:
+    with open(input_file, 'r') as f:
         input_str = f.read()
 
     try:
@@ -244,7 +244,7 @@ def compile_to_css(input_file):
             )
         )
     except sass.CompileError as e:
-        logging.error("Failed to compile {}:\n{}".format(input_file, e))
+        logging.error('Failed to compile {}:\n{}'.format(input_file, e))
     return ""
 
 
@@ -253,7 +253,7 @@ def compile_to_css_and_save(input_file, dest_file):
     if dest_file:
         with open(dest_file, 'w') as css_file:
             css_file.write(stylesheet)
-            logger.info("Created CSS file {}".format(dest_file))
+            logger.info('Created CSS file {}'.format(dest_file))
     else:
         print(stylesheet)
 
@@ -297,21 +297,21 @@ class SourceModificationEventHandler(FileSystemEventHandler):
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="QtSASS",
-        description="Compile a Qt compliant CSS file from a SASS stylesheet.",
+        prog='QtSASS',
+        description='Compile a Qt compliant CSS file from a SASS stylesheet.',
     )
-    parser.add_argument('input', type=str, help="The SASS stylesheet file.")
+    parser.add_argument('input', type=str, help='The SASS stylesheet file.')
     parser.add_argument(
         '-o',
         '--output',
         type=str,
-        help="The path of the generated Qt compliant CSS file."
+        help='The path of the generated Qt compliant CSS file.'
     )
     parser.add_argument(
         '-w',
         '--watch',
         action='store_true',
-        help="If set, recompile when the source file changes."
+        help='If set, recompile when the source file changes.'
     )
 
     args = parser.parse_args()
@@ -324,7 +324,7 @@ def main():
             args.output,
             watched_dir
         )
-        logging.info("qtsass is watching {}...".format(args.input))
+        logging.info('qtsass is watching {}...'.format(args.input))
         observer = Observer()
         observer.schedule(event_handler, watched_dir, recursive=False)
         observer.start()
