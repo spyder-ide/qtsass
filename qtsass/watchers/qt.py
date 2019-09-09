0fb7c6ec41cf0718b0b452ca0a222e2105bc4b1c
Dan Bradham <danielbradham@gmail.com>
2019-09-09 08:58:01 -0400

    QtWatcher subclasses PollingWatcher - dispatches compile and callbacks in main ui thread   when a QApplication has started otherwise behaves   exactly like a PollingWatcher

--- qtsass/watchers/qt.py
+++ qtsass/watchers/qt.py
@@ -12,70 +12,85 @@
 
 from __future__ import absolute_import
 
-# Third party imports
+# Local imports
+from qtsass.watchers.polling import PollingWatcher
+
+
+# We cascade through Qt bindings here rather than relying on a comprehensive
+# Qt compatability library like qtpy or Qt.py. This prevents us from forcing a
+# specific compatability library on users.
 QT_BINDING = None
 if not QT_BINDING:
     try:
         from PySide2.QtWidgets import QApplication
-        from PySide2.QtCore import QFileSystemWatcher
+        from PySide2.QtCore import QObject, Signal
         QT_BINDING = 'pyside2'
     except ImportError:
         pass
 if not QT_BINDING:
     try:
         from PyQt5.QtWidgets import QApplication
-        from PyQt5.QtCore import QFileSystemWatcher
+        from PyQt5.QtCore import QObject
+        from PyQt5.QtCore import pyqtSignal as Signal
         QT_BINDING = 'pyqt5'
     except ImportError:
         pass
 if not QT_BINDING:
     try:
         from PySide.QtGui import QApplication
-        from PySide.QtCore import QFileSystemWatcher
+        from PySide2.QtCore import QObject, Signal
         QT_BINDING == 'pyside'
     except ImportError:
         pass
 if not QT_BINDING:
     from PyQt4.QtGui import QApplication
-    from PyQt4.QtCore import QFileSystemWatcher
+    from PyQt4.QtCore import QObject
+    from PyQt4.QtCore import pyqtSignal as Signal
     QT_BINDING == 'pyqt4'
 
-# Local imports
-from qtsass.watchers.api import Watcher
 
 # yapf: enable
 
 
-# TODO: This doesn't work yet.
+class QtDispatcher(QObject):
+    """Used by QtWatcher to dispatch callbacks in the main ui thread."""
+
+    signal = Signal()
 
 
-class QtWatcher(Watcher):
-    """The Qt implementation of the Watcher api. Uses a QFileSystemWatcher
-    to monitor the filesystem.
+class QtWatcher(PollingWatcher):
+    """The Qt implementation of the Watcher api.
+
+    Subclasses PollingWatcher but dispatches :meth:`compile_and_dispatch`
+    using a Qt Signal to ensure that these calls are executed in the main ui
+    thread. We aren't using a QFileSystemWatcher because it fails to report
+    changes in certain circumstances.
     """
+
     _qt_binding = QT_BINDING
 
     def setup(self):
-        self._qapp = QApplication.instance()
-        if not self._qapp:
-            raise RuntimeError('QtWatcher created before QApplication.')
+        """Setup QtWatcher"""
+        super(QtWatcher, self).setup()
+        self._qtdispatcher = None
 
-        self._qfswatcher = QFileSystemWatcher()
+    @property
+    def qtdispatcher(self):
+        """Get the QtDispatcher"""
+        if self._qtdispatcher is None:
+            self._qtdispatcher = QtDispatcher()
+            self._qtdispatcher.signal.connect(self.compile_and_dispatch)
+        return self._qtdispatcher
 
-    def connect(self, fn):
-        self._qfswatcher.fileChanged.connect(fn)
-        self._qfswatcher.directoryChanged.connect(fn)
+    def on_change(self):
+        """Called when a change is detected in _check_snapshot."""
+        self._log.debug('Change detected...')
 
-    def disconnect(self, fn):
-        self._qfswatcher.fileChanged.disconnect(fn)
-        self._qfswatcher.directoryChanged.disconnect(fn)
+        # If a QApplication event loop has not been started
+        # call compile_and_dispatch in the current thread.
+        if not QApplication.instance():
+            return super(PollingWatcher, self).compile_and_dispatch()
 
-    def start(self):
-        self._qfswatcher.addPath(self._watch_dir)
-
-    def stop(self):
-        self._qfswatcher.removePath(self._watch_dir)
-        self._qfswatcher = None
-
-    def join(self):
-        """Nothing to join here. Unlike PollingWatcher and WatchdogWatcher."""
+        # Create and use a QtDispatcher to ensure compile and any
+        # connected callbacks get executed in the main gui thread.
+        self.qtdispatcher.signal.emit()
