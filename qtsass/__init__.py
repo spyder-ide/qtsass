c4a2ac4ab475ab0bfbad2543939df8abb0412836
dalthviz <d.althviz10@uniandes.edu.co>
2022-09-16 11:40:08 -0500

    Back to work

--- qtsass/__init__.py
+++ qtsass/__init__.py
@@ -41,7 +41,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 enable_logging()
 
 # Constants
-__version__ = '0.3.2'
+__version__ = '0.4.0.dev0'
 
 
 def _to_version_info(version):
